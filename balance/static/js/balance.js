f40f32bf1b863f5442eb5e6deb7f019316a3ccd9
NICO9 <nmmendez9@hotmail.com>
2024-11-27 00:26:44 +0400

    feat: agregamos los movimientos a la tabla

--- balance/static/js/balance.js
+++ balance/static/js/balance.js
@@ -5,7 +5,38 @@ function cargarMovimientos() {
     console.log('Has llamado a la función para cargar los movimientos');
     peticion.open('GET', 'http://127.0.0.1:5000/api/v1/movimientos', false);
     peticion.send();
-    console.log('Respuesta de la API', peticion.status, peticion.statusText, peticion.responseText)
+    const resultado = JSON.parse(peticion.responseText);
+    const movimientos = resultado.results;
+
+    let html = '';
+    /*
+    En Python:
+        for i in range(len(movimientos)):
+            movimientos[i]
+            i = i + 1
+    */
+
+    for (let i = 0; i < movimientos.length; i++) {
+        const mov = movimientos[i];
+        html = html + `
+            <tr class="fila">
+                <td class="dato">${mov.fecha}</td>
+                <td class="dato">${mov.concepto}</td>
+                <td class="dato">${mov.tipo}</td>
+                <td class="dato">${mov.cantidad}</td>
+                <td class="acciones">
+                    <a href="/editar/${mov.id}" class="mini-boton">
+                        <img src="/static/icons/editar.png" alt="Editar">
+                    </a>
+                    <a href="/eliminar/${mov.id}" class="mini-boton">&times;</a>
+                </td>
+            </tr>
+        `;
+    }
+    console.log('html', html);
+    const tabla = document.querySelector('#cuerpo-tabla');
+    tabla.innerHTML = html;
+
 }
 
 window.onload = function () {

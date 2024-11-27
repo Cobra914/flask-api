f2634d6aa29abdfdbd2c9589024665dc855f1ee8
NICO9 <nmmendez9@hotmail.com>
2024-11-27 23:48:03 +0400

    feat: instalamos flask-cors

--- balance/__init__.py
+++ balance/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
+
+
 ALMACEN = 1   # 0 - CSV, 1 - DB
 
 app = Flask(__name__)
 app.config.from_prefixed_env()
+cors = CORS(app, resources={
+    r'/api/*': {
+        'origins': '*'
+    }
+})
 
 print('***** VARIABLES DE ENTORNO *****')
 print('DEBUG', app.config['DEBUG'])
